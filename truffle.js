1b290d8d45d9211eb0458e1a3f363b8aebefce9e
rohithrajasekharan <rohithrajasekharan@outlook.com>
2018-12-04 22:34:07 +0530

    compiled after unbox

---
+++ truffle.js
@@ -0,0 +1,13 @@
+
+const path = require("path");
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: "8545",
+      network_id: "*"
+    }
+  },
+  contracts_build_directory: path.join(__dirname, "client/src/contracts")
+};
